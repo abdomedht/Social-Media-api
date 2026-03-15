c13a9c3a776868001a992df0c4791c57c686252e
abdo medhat <90509944+abdomedht@users.noreply.github.com>
2026-03-15 16:26:22 +0200

    add post model and create post

--- index.js
+++ index.js
@@ -5,11 +5,11 @@
  */
 import  bootstrap  from './src/app.controller.js';
 import  express  from 'express';
+import process from 'node:process';
 import path from 'node:path';
 import * as dotenv from 'dotenv';
 dotenv.config({path:path.resolve('./src/config/.env.dev')});
-const app = express()
-const port = process.env.PORT||3000
-
-bootstrap(app , express)
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express();
+const port = process.env.PORT||3000;
+bootstrap(app , express);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
